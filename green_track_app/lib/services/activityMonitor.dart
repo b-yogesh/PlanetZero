9d399868357861bc542810bfd2efea6a97490559
Simon Farshid <Yonom@users.noreply.github.com>
2020-09-26 18:41:17 +0200

    Add measurements to database

--- green_track_app/lib/services/activityMonitor.dart
+++ green_track_app/lib/services/activityMonitor.dart
@@ -1,7 +1,9 @@
 import 'package:activity_recognition_flutter/activity_recognition_flutter.dart';
 import 'package:flutter/cupertino.dart';
-import 'dart:developer';
 import 'package:geolocator/geolocator.dart';
+import 'package:green_track_app/models/MeasuredActivity.dart';
+import 'package:green_track_app/models/MeasuredLocation.dart';
+import 'package:green_track_app/services/database.dart';
 
 void startActivityMonitor() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,19 +12,38 @@ void startActivityMonitor() {
   ActivityRecognition.activityUpdates().listen(onActivityData);
 
   // Start the position stream updates
-  getPositionStream(desiredAccuracy: LocationAccuracy.high).listen(onPositionData);
+  getPositionStream(desiredAccuracy: LocationAccuracy.high)
+      .listen(onPositionData);
 }
 
-void onActivityData(Activity activity) {
+void onActivityData(Activity activity) async {
   // Do something with the activity
   var type = activity.type;
   var confidence = activity.confidence;
 
-  log(type.toString() + " " + confidence.toString());
+  var measuredActivity = MeasuredActivity(
+      confidence: confidence, type: type.toString(), timestamp: DateTime.now());
+  await insertMeasuredActivity(measuredActivity);
 }
 
-void onPositionData(Position position) {
-  print(position == null
-      ? 'Unknown'
-      : position.latitude.toString() + ', ' + position.longitude.toString());
+void onPositionData(Position position) async {
+  var accuracy = position.accuracy;
+  var altitude = position.altitude;
+  var heading = position.heading;
+  var latitude = position.latitude;
+  var longitude = position.longitude;
+  var speed = position.speed;
+  var speedAccuracy = position.speedAccuracy;
+
+  var measuredLocation = MeasuredLocation(
+    accuracy: accuracy,
+    altitude: altitude,
+    heading: heading,
+    latitude: latitude,
+    longitude: longitude,
+    speed: speed,
+    speedAccuracy: speedAccuracy,
+    timestamp: DateTime.now(),
+  );
+  await insertMeasuredLocation(measuredLocation);
 }
