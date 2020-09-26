7efb1e630b06f2e82793f387013454d2a95c14b7
Simon Farshid <Yonom@users.noreply.github.com>
2020-09-26 18:10:35 +0200

    Create a bunch of tables

--- green_track_app/lib/services/database.dart
+++ green_track_app/lib/services/database.dart
@@ -2,18 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+Future<Database> database;
+
 Future<void> initDatabase() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Open the database and store the reference.
-  final Future<Database> database = openDatabase(
+  database = openDatabase(
     join(await getDatabasesPath(), 'database.db'),
     onCreate: (db, version) {
       return db.execute(
         """
-        CREATE TABLE activities(
+        CREATE TABLE measuredActivities (
           id INTEGER PRIMARY KEY, 
-          name TEXT, 
-          age INTEGER
+          type TEXT,
+          confidence INTEGER,
+          timestamp TEXT
+        );
+
+        CREATE TABLE measuredLocations (
+          id INTEGER PRIMARY KEY, 
+          latitude REAL,
+          longtitude REAL,
+          altitude REAL,
+          heading REAL,
+          accuracy REAL,
+          timestamp TEXT
+        );
+
+        CREATE TABLE activities (
+          id INTEGER PRIMARY KEY, 
+          type TEXT,
+          start TEXT,
+          end TEXT,
+          distance TEXT
         );
         """,
       );
