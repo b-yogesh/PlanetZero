86bbc6d2616fc9506dfa4063ff9df6afaad3c57a
Simon Farshid <Yonom@users.noreply.github.com>
2020-09-27 08:42:16 +0200

    add infobadgeinverted

--- green_track_app/lib/widgets/infoBadge.dart
+++ green_track_app/lib/widgets/infoBadge.dart
@@ -26,7 +26,8 @@ class InfoBadgeNumber extends StatelessWidget {
             Countup(
               begin: 0,
               end: wholePart.toDouble(),
-              duration: Duration(milliseconds: min(wholePart * durationMultiplier, 850)),
+              duration: Duration(
+                  milliseconds: min(wholePart * durationMultiplier, 850)),
               separator: '.',
               style: infoBadgeNumberStyle,
             ),
@@ -35,8 +36,8 @@ class InfoBadgeNumber extends StatelessWidget {
               Countup(
                 begin: 0,
                 end: decimalPart.toDouble(),
-                duration:
-                    Duration(milliseconds: min(decimalPart * durationMultiplier, 850)),
+                duration: Duration(
+                    milliseconds: min(decimalPart * durationMultiplier, 850)),
                 separator: '.',
                 style: infoBadgeNumberStyle,
               ),
@@ -78,6 +79,47 @@ class InfoBadge extends StatelessWidget {
             style: infoBadgeSuffixStyle,
           ),
         )
+      ],
+    );
+  }
+}
+
+class InfoBadgeInverted extends StatelessWidget {
+  final int wholePart;
+  final int decimalPart;
+  final bool useDecimal;
+  final String suffix;
+
+  InfoBadgeInverted({
+    this.wholePart,
+    this.decimalPart,
+    this.useDecimal,
+    this.suffix,
+  });
+
+  final TextStyle infoBadgeSuffixStyle = TextStyle(
+    fontSize: 18,
+    fontFamily: "Nunito Sans",
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            suffix,
+            style: infoBadgeSuffixStyle,
+          ),
+        ),
+        InfoBadgeNumber(
+          wholePart: wholePart,
+          decimalPart: decimalPart,
+          useDecimal: useDecimal,
+        ),
       ],
     );
   }
